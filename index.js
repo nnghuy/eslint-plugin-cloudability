493ba5d283f1ff51a10c097c200c40c64d1d6f35
Huy Nguyen <huy.nguyen@cloudability.com>
2017-01-05 11:54:15 -0800

    First commit

---
+++ index.js
@@ -0,0 +1,23 @@
+/* eslint-disable global-require */
+
+'use strict';
+
+var allRules = {
+  'prefer-callback-set-state': require('.lib/rules/prefer-callback-set-state')
+}
+
+module.exports = {
+  rules: allRules ,
+  configs: {
+    recommended: {
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true
+        }
+      },
+      rules: {
+        'cloudability/prefer-callback-set-state': 'error',
+      }
+    }
+  }
+};
